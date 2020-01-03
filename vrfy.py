0f99dc5257b41d4d7d22a2c6e135b1f6bf68b9c6
omertacs <38165246+omertacs@users.noreply.github.com>
2020-01-03 02:50:09 -0700

    Update vrfy.py

--- vrfy.py
+++ vrfy.py
@@ -11,10 +11,15 @@ with open('users.txt', 'r') as f:
         users = f.readlines() # creates an array of users ["user1", "user2", ...etc"]
         
         
+'''
+TODO: Make server and file user input
 
-#if len(sys.argv) != 2:
-#       print("Usage: vrfy.py <usernames>"
-#       sys.exit(0)
+if len(sys.argv) != 2:
+print("Usage: vrfy.py <usernames>"
+sys.exit(0)
+
+'''
+
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket
 connect=s.connect(('10.11.1.227',25)) #connect to the server
